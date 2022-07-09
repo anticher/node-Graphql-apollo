87294878c94db7cca0cdaecb532de62917a2ab8c
anticher <anticherq@gmail.com>
2022-07-09 13:02:57 +0300

    feat: implement favourites resolvers nesting and typing

--- app/modules/favourites/resolvers/favourites.js
+++ app/modules/favourites/resolvers/favourites.js
@@ -1,7 +1,54 @@
 export const resolvers = {
     Query: {
-        favourites: async (_, { }, { dataSources }) => {
-            return dataSources.favouritesAPI.getFavourites()
+        favourites: async (_, { }, context) => {
+            return context.dataSources.favouritesAPI.getFavourites(context)
+        },
+    },
+    Favourite: {
+        id: ({ _id }) => _id,
+        bands: async (root, args, { dataSources }) => {
+            const { bandsIds } = root
+            const promises = bandsIds.map((id) => {
+                return dataSources.bandsAPI.getBand(id)
+            })
+            const bands = []
+            await Promise.all(promises).then((results) => {
+                results.forEach((res) => bands.push(res))
+            })
+            return bands
+        },
+        artists: async (root, args, { dataSources }) => {
+            const { artistsIds } = root
+            const promises = artistsIds.map((id) => {
+                return dataSources.artistsAPI.getArtist(id)
+            })
+            const artists = []
+            await Promise.all(promises).then((results) => {
+                results.forEach((res) => artists.push(res))
+            })
+            return artists
+        },
+        genres: async (root, args, { dataSources }) => {
+            const { genresIds } = root
+            const promises = genresIds.map((id) => {
+                return dataSources.genresAPI.getGenre(id)
+            })
+            const genres = []
+            await Promise.all(promises).then((results) => {
+                results.forEach((res) => genres.push(res))
+            })
+            return genres
+        },
+        tracks: async (root, args, { dataSources }) => {
+            const { trackIds } = root
+            const promises = trackIds.map((id) => {
+                return dataSources.tracksAPI.getTrack(id)
+            })
+            const tracks = []
+            await Promise.all(promises).then((results) => {
+                results.forEach((res) => tracks.push(res))
+            })
+            return tracks
         },
     },
     Mutation: {
--- app/modules/favourites/services/favourites.js
+++ app/modules/favourites/services/favourites.js
@@ -6,15 +6,20 @@ export class FavouritesAPI extends RESTDataSource {
         this.baseURL = 'http://localhost:3007/v1/favourites'
     }
 
-    async getFavourites() {
-        console.log('h')
-        const response = await this.get('')
-        console.log(response)
-        const resultArr = []
-        for (const key in response.items) {
-            resultArr.push(response.items[key])
-        }
-        return resultArr
+    async getFavourites(context) {
+        const authToken = context.authToken
+        const response = await this.get('', {}, {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: authToken,
+            },
+        })
+        // const resultArr = []
+        // for (const key in response.items) {
+        //     resultArr.push(response.items[key])
+        // }
+        // console.log(resultArr)
+        return response
     }
 
     async addFavourite(input, context) {
